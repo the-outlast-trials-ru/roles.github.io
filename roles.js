fb4ca134a0655829bfb47cd14f904a958167657f
the-outlast-trials-ru <theoutlasttrialsru@gmail.com>
2026-01-21 17:47:33 +0500

    Add files via upload

---
+++ roles.js
@@ -0,0 +1,16 @@
+const roles = [
+  {
+"name": "fdsfsdfsfDFDSFSDF 😜😜😜",
+    "description": "выавыа",
+    "category": "ыва",
+    "color": "#da2b2b",
+    "gradient": "linear-gradient(135deg, #5865F2, #404EED)"
+  },
+  {
+"name": "sdfdsfdsf",
+    "description": "hjihjihji",
+    "category": "hjihjihji",
+    "color": "#0c4897",
+    "gradient": "linear-gradient(135deg, #FF00FF, #8A2BE2)"
+  }
+];
