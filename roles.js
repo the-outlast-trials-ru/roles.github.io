64ad06f5dcf74125cfdc6c48afa4882901e23fcb
the-outlast-trials-ru <theoutlasttrialsru@gmail.com>
2026-01-21 18:23:57 +0500

    Update roles.js

--- roles.js
+++ roles.js
@@ -1,15 +1,15 @@
 const roles = [
   {
-"name": "fdsfsdfsfDFDSFSDF 😜😜😜",
-    "description": "выавыа",
-    "category": "ыва",
+"name": "Легенда 😜😜😜",
+    "description": "За -------",
+    "category": "Достижения",
     "color": "#da2b2b",
     "gradient": "linear-gradient(135deg, #5865F2, #404EED)"
   },
   {
-"name": "sdfdsfdsf",
-    "description": "hjihjihji",
-    "category": "hjihjihji",
+"name": "Гемофилик",
+    "description": "За ........",
+    "category": "Кастомные роли",
     "color": "#0c4897",
     "gradient": "linear-gradient(135deg, #FF00FF, #8A2BE2)"
   }
