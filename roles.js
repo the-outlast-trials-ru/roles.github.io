9ead2ae455469eec474f215c8d38d18ea5cbc94c
the-outlast-trials-ru <theoutlasttrialsru@gmail.com>
2026-01-23 22:30:40 +0500

    Update roles.js

--- roles.js
+++ roles.js
@@ -1,12 +1,5 @@
 const roles = [
   {
-    "name": "Легенда",
-    "description": "За -------",
-    "category": "Основные роли",
-    "color": "#da2b2b",
-    "gradient": "linear-gradient(135deg, #5865F2, #404EED)"
-  },
-  {
     "name": "Гемофилик",
     "description": "За ........",
     "category": "Кастомные роли",
@@ -14,31 +7,10 @@ const roles = [
     "gradient": "linear-gradient(135deg, #FF00FF, #8A2BE2)"
   },
   {
-    "name": "Старший администратор",
-    "description": "За ........",
-    "category": "Администрация и модерация",
-    "color": "#0c4897",
-    "gradient": "linear-gradient(135deg, #FF00FF, #8A2BE2)"
-  },
-  {
-    "name": "Главный администратор",
-    "description": "За ........",
-    "category": "Администрация и модерация",
-    "color": "#0c4897",
-    "gradient": "linear-gradient(135deg, #FF00FF, #8A2BE2)"
-  },
-  {
-    "name": "ывавыа",
-    "description": "ывавыавыа",
-    "category": "Ивентные роли",
-    "color": "#6666ff",
-    "gradient": "linear-gradient(135deg, #6666ff 0%, #3333cc 100%)"
-  },
-  {
-    "name": "gdfgdf",
-    "description": "gdfgdfg",
-    "category": "Платные роли",
-    "color": "#99ff33",
-    "gradient": "linear-gradient(135deg, #99ff33 0%, #66cc00 100%)"
+    "name": "666",
+    "description": "ыВАавпявпф",
+    "category": "Особые",
+    "color": "#ffcc00",
+    "gradient": "linear-gradient(135deg, #ffcc00 0%, #cc9900 100%)"
   }
 ];
