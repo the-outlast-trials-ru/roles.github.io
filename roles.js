b4f60d9afe6ee1809d9e624fd87af17a48d5a0e3
the-outlast-trials-ru <theoutlasttrialsru@gmail.com>
2026-01-23 15:19:21 +0500

    Update roles.js

--- roles.js
+++ roles.js
@@ -1,30 +1,37 @@
 const roles = [
   {
-"name": "Легенда",
+    "name": "Легенда",
     "description": "За -------",
     "category": "Достижения",
     "color": "#da2b2b",
     "gradient": "linear-gradient(135deg, #5865F2, #404EED)"
   },
   {
-"name": "Гемофилик",
+    "name": "Гемофилик",
     "description": "За ........",
     "category": "Кастомные роли",
     "color": "#0c4897",
     "gradient": "linear-gradient(135deg, #FF00FF, #8A2BE2)"
   },
-    {
-"name": "Старший администратор",
+  {
+    "name": "Старший администратор",
     "description": "За ........",
     "category": "Администрация",
     "color": "#0c4897",
     "gradient": "linear-gradient(135deg, #FF00FF, #8A2BE2)"
   },
-    {
-  "name": "Главный администратор",
+  {
+    "name": "Главный администратор",
     "description": "За ........",
     "category": "Администрация",
     "color": "#0c4897",
     "gradient": "linear-gradient(135deg, #FF00FF, #8A2BE2)"
+  },
+  {
+    "name": "ывавыа",
+    "description": "ывавыавыа",
+    "category": "Модерация",
+    "color": "#6666ff",
+    "gradient": "linear-gradient(135deg, #6666ff 0%, #3333cc 100%)"
   }
 ];
