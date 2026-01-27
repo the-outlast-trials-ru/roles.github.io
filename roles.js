4ae894e3dcc0589608f6a4f72b89464ddec856df
the-outlast-trials-ru <theoutlasttrialsru@gmail.com>
2026-01-27 11:29:07 +0500

    Update roles.js

--- roles.js
+++ roles.js
@@ -5,5 +5,12 @@ const roles = [
     "category": "🛡️ Администрация",
     "color": "#da2b2b",
     "gradient": "linear-gradient(135deg, #ff3333 0%, #cc0000 100%)"
+  },
+  {
+    "name": "Егерь",
+    "description": "за ...........",
+    "category": "🎉 Ивент роли",
+    "color": "#8A2BE2",
+    "gradient": "linear-gradient(135deg, #ff3333 0%, #cc0000 100%)"
   }
 ];
