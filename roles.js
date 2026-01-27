f5183551439a2071a990150a7c9e2ffdbea54c1f
the-outlast-trials-ru <theoutlasttrialsru@gmail.com>
2026-01-27 11:51:20 +0500

    Update roles.js

--- roles.js
+++ roles.js
@@ -10,7 +10,35 @@ const roles = [
     "name": "Егерь",
     "description": "за ...........",
     "category": "🎉 Ивент роли",
-    "color": "#9933ff",
-    "gradient": "linear-gradient(135deg, #9933ff 0%, #6600cc 100%)"
+    "color": "#8A2BE2",
+    "gradient": "linear-gradient(135deg, #ff3333 0%, #cc0000 100%)"
+  },
+  {
+    "name": "Season 5",
+    "description": "За ..........",
+    "category": "🏆 Достижения",
+    "color": "#ffcc00",
+    "gradient": "linear-gradient(135deg, #ffcc00 0%, #cc9900 100%)"
+  },
+  {
+    "name": "VIP Basic",
+    "description": "За ........",
+    "category": "💎 Платные / VIP роли",
+    "color": "#cccccc",
+    "gradient": "linear-gradient(135deg, #cccccc 0%, #999999 100%)"
+  },
+  {
+    "name": "Гемофилик",
+    "description": "За .........",
+    "category": "✨ Бонусные роли",
+    "color": "#ffffff",
+    "gradient": "linear-gradient(135deg, #ffffff 0%, #e6e6e6 100%)"
+  },
+  {
+    "name": "Кошмар реагентов",
+    "description": "За .........",
+    "category": "🎭 Кастомные роли",
+    "color": "#333333",
+    "gradient": "linear-gradient(135deg, #333333 0%, #000000 100%)"
   }
 ];
