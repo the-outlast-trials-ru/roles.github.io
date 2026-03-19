7333424da8757c9f3cbc9dff819904877f55e89d
the-outlast-trials-ru <theoutlasttrialsru@gmail.com>
2026-03-20 00:14:44 +0500

    Update members.js

--- members.js
+++ members.js
@@ -1,6 +1,9 @@
 const membersData = {
-  "Администрация": [
-    { name: "Петров Петр Петрович", nickname: "petrov_pp", roles: 8 },
-    { name: "Иванов Иван Иванович", nickname: "ivanov_ii", roles: 12 }
+  "Команда проекта": [
+    { name: "berryholls", nickname: "berryholls", roles: 7 },
+    { name: "Despectr|Trial Administration", nickname: "ivanov_ii", roles: 18 },
+    { name: "CL Support", nickname: "ivanov_ii", roles: 2 },
+    { name: "✭", nickname: "cult_of_nightmare", roles: 13 },
+    { name: "Source", nickname: "warlordxdd", roles: 3 }
   ]
 };
