11e77f42c50886fd28aa958c1b4255530155c6c4
the-outlast-trials-ru <theoutlasttrialsru@gmail.com>
2026-03-20 00:15:34 +0500

    Update news.js

--- news.js
+++ news.js
@@ -1,8 +1,1 @@
-const news = [
-  {
-    "title": "НЕБОЛЬШОЕ ОБНОВЛЕНИЕ ИЛИ СПИСОК ИЗМЕНЕНИЙ",
-    "content": "A new hotfix patch was released earlier today featuring the following fixes:\n\n● Improved Imposter knife attack angles when engaging Reagents at close range.\n\nFixed an issue where the Regroup button could trigger automatically if a player was killed outside the Regroup Room while playing solo.\n\nFixed an issue where Imposters could cancel the end of the Slash animation to bypass its cooldown (again 🤞).\n\nFixed an issue where Task exclamation mark icons could remain stuck on screen.\n\nFixed an issue where Reagents could become unable to complete a Trial after entering the SAS door while downed.\n\nVarious additional bug fixes and improvements.\n\nFeel free to join our official 18+ Outlast Discord Server and open a '/ticket' to speak directly with our support team about any issues you encounter in-game.\n\nYou can also fill out our General Feedback Survey, or if you played the previous season, Project Diarchy, fill out our Season 4: Project Diarchy Player Sentiment Survey. For any information on our Twitch Drops and Account Linking please visit this page.",
-    "category": "hotfix",
-    "date": "2026-03-19T16:05:52.331Z"
-  }
-];
+
