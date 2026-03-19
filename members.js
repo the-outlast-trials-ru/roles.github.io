c7d4df7521e212eff2e17fadc91a90c4cce90136
the-outlast-trials-ru <theoutlasttrialsru@gmail.com>
2026-03-20 03:04:06 +0500

    Update members.js

--- members.js
+++ members.js
@@ -5,8 +5,7 @@ const membersData = {
     { name: "CL Support", nickname: "miuuu0905", roles: 2 },
     { name: "✭", nickname: "cult_of_nightmare", roles: 13 },
     { name: "Source", nickname: "warlordxdd", roles: 3 }
-  ]
-};
+  ],
   "[1000+]🔪": [
     { name: "Bosko Trial French Baguette", nickname: "bosko861", roles: 8 },
     { name: "Despectr | Trial Administration", nickname: "despectr", roles: 18 },
@@ -14,8 +13,7 @@ const membersData = {
     { name: "Isaac", nickname: "isaacgreene", roles: 6 },
     { name: "lili | Trial Administration", nickname: "lili____", roles: 22 },
     { name: "Olivka", nickname: "olivka6666", roles: 9 }
-  ]
-};
+  ],
   "[100+]🔪": [
     { name: "?CвЯтoЙ¿", nickname: "reaject", roles: 9 },
     { name: "Ветер", nickname: "lesnitskiy", roles: 6 },
