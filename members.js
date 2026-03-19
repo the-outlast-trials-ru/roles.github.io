ec68b25edc123d67355d69a114f7f545aaa60186
the-outlast-trials-ru <theoutlasttrialsru@gmail.com>
2026-03-20 02:55:39 +0500

    Add files via upload

---
+++ members.js
@@ -0,0 +1,27 @@
+const membersData = {
+  "Команда проекта": [
+    { name: "berryholls", nickname: "berryholls", roles: 7 },
+    { name: "Despectr | Trial Administration", nickname: "despectr", roles: 18 },
+    { name: "CL Support", nickname: "miuuu0905", roles: 2 },
+    { name: "✭", nickname: "cult_of_nightmare", roles: 13 },
+    { name: "Source", nickname: "warlordxdd", roles: 3 }
+  ]
+};
+  "[1000+]🔪": [
+    { name: "Bosko Trial French Baguette", nickname: "bosko861", roles: 8 },
+    { name: "Despectr | Trial Administration", nickname: "despectr", roles: 18 },
+    { name: "Divine", nickname: "_divine9_", roles: 12 },
+    { name: "Isaac", nickname: "isaacgreene", roles: 6 },
+    { name: "lili | Trial Administration", nickname: "lili____", roles: 22 },
+    { name: "Olivka", nickname: "olivka6666", roles: 9 }
+  ]
+};
+  "[100+]🔪": [
+    { name: "?CвЯтoЙ¿", nickname: "reaject", roles: 9 },
+    { name: "Ветер", nickname: "lesnitskiy", roles: 6 },
+    { name: "шейх", nickname: "oleg05952", roles: 4 },
+    { name: "OZICHokTTV", nickname: "oz1ch", roles: 17 },
+    { name: "RusianAkcher", nickname: "rusianachker", roles: 2 },
+    { name: "w3ferens", nickname: "w3ferens", roles: 9 }
+  ]
+}
