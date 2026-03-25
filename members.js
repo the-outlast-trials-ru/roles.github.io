d14cc77e5ff2f268715b527617d222132439f153
the-outlast-trials-ru <theoutlasttrialsru@gmail.com>
2026-03-25 14:38:53 +0500

    Update members.js

--- members.js
+++ members.js
@@ -1,11 +1,11 @@
 
 const membersData = {
   "Команда проекта": [
-    { name: "berryholls", nickname: "berryholls", roles: 7, place: 1 },
-    { name: "Despectr | Trial Administration", nickname: "despectr", roles: 18, place: 2 },
-    { name: "CL Support", nickname: "miuuu0905", roles: 2, place: 3 },
-    { name: "✭", nickname: "cult_of_nightmare", roles: 13, place: 4 },
-    { name: "Source", nickname: "warlordxdd", roles: 3, place: 5 }
+    { name: "berryholls", nickname: "berryholls", roles: 7 },
+    { name: "Despectr | Trial Administration", nickname: "despectr", roles: 18 },
+    { name: "CL Support", nickname: "miuuu0905", roles: 2 },
+    { name: "✭", nickname: "cult_of_nightmare", roles: 13 },
+    { name: "Source", nickname: "warlordxdd", roles: 3 }
   ],
   "[1000+]🔪": [
     { name: "Bosko Trial French Baguette", nickname: "bosko861", roles: 8 },
